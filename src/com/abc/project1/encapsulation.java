1cf5a6001e99314753463b7fdf20d35abd4a6b8d
mroien <oien.tim@gmail.com>
2015-09-20 13:40:05 -0500

    reordered whole project and made default classes

---
+++ src/com/abc/project1/encapsulation.java
@@ -0,0 +1,21 @@
+package com.abc.project1;
+
+/**
+ * Created by mroien on 9/20/15.
+ */
+import java.util.Scanner;
+
+public class encapsulation {
+    public static void main(String arg[]) {
+
+        System.out.println("Enter a String");
+        Scanner scan = new Scanner(System.in);
+        String data = scan.nextLine();
+        System.out.println("You entered " + data);
+        Scanner scan2 = new Scanner(System.in);
+        String data2 = scan2.nextLine();
+        System.out.println("New next is " + data + " " +data2);
+    }
+    public void protocol1(String[] data){
+    }
+}
---
+++ src/com/abc/project4/main.java
@@ -0,0 +1,7 @@
+package com.abc.project4;
+
+/**
+ * Created by mroien on 9/20/15.
+ */
+public class main {
+}
