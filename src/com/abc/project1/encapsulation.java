be901bc29f69c8497c3d6fb3f2f7411dd38aa1f3
mroien <oien.tim@gmail.com>
2015-09-21 17:26:39 -0500

    project 1 finished

--- src/com/abc/project1/encapsulation.java
+++ src/com/abc/project1/encapsulation.java
@@ -3,19 +3,56 @@ package com.abc.project1;
 /**
  * Created by mroien on 9/20/15.
  */
-import java.util.Scanner;
 
 public class encapsulation {
     public static void main(String arg[]) {
-
-        System.out.println("Enter a String");
-        Scanner scan = new Scanner(System.in);
-        String data = scan.nextLine();
-        System.out.println("You entered " + data);
-        Scanner scan2 = new Scanner(System.in);
-        String data2 = scan2.nextLine();
-        System.out.println("New next is " + data + " " +data2);
+        String packet = "data";
+        protocol1(packet);
     }
-    public void protocol1(String[] data){
+
+    public static void protocol1(String data) {
+        String data2 = "protocol1 " + data;
+        System.out.println(data2);
+        protocol2(data2);
+    }
+
+    public static void protocol2(String data) {
+        String data2 = "protocol2 " + data;
+        System.out.println(data2);
+        protocol3(data2);
+
+    }
+
+    public static void protocol3(String data) {
+        String data2 = "protocol3 " + data;
+        System.out.println(data2);
+        protocol4(data2);
+
+    }
+
+    public static void protocol4(String data) {
+        String data2 = "protocol4 " + data;
+        System.out.println(data2);
+        protocol5(data2);
+
+    }
+
+    public static void protocol5(String data) {
+        String data2 = "protocol5 " + data;
+        System.out.println(data2);
+        protocol6(data2);
+
+    }
+
+    public static void protocol6(String data) {
+        String data2 = "protocol6 " + data;
+        System.out.println(data2);
+        protocol7(data2);
+
+    }
+
+    public static void protocol7(String data) {
+        String data2 = "protocol7 " + data;
+        System.out.println(data2);
     }
 }
